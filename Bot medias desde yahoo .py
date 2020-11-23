1f8d3d947d1a7be7952c6818269ede5275c8801b
PatricioHenderson <72527088+PatricioHenderson@users.noreply.github.com>
2020-11-23 14:28:29 -0300

    Todas las acciones

--- Bot medias desde yahoo .py
+++ Bot medias desde yahoo .py
@@ -12,19 +12,74 @@ Programa que analiza si hubo cruce de medias moviles
 
 __author__ = "Patricio Henderson"
 __email__ = "patriciohenderson@hotmail.com"
-__version__ = "1.3"
+__version__ = "1.4"
 
 
 import csv
 import pandas_datareader as pdr
 import statistics
 
-precio_veinte = 0
-precio_doce = 0
+#Iniciamos contadores en 0
+
+
+def analisis (empresa):
+    precio_dieciocho = 0
+    precio_nueve = 0
+    nombre = empresa + "BA.csv"
+    with open (nombre) as fo:
+    #Abrimos archivo .csv    
+
+        file = list(csv.DictReader(fo))
+        #leemos en forma de lista
+        len_reader = len(file)
+    
+        ultimas_dieciocho = len_reader - 18
+        #Restaos para obtener las últimas 18 y poder sacar la media
+        try:
+            for i in range(ultimas_dieciocho,len_reader):
+
+                row = file[i]
+                #arrancamos desde las últimas 20 filas
+                precio_cierre = float(row.get("Close"))
+                #obtenemos el precio de cierre
+                precio_dieciocho += precio_cierre
+                #Vamos sumando el precio de cierre
+            promedio_dieciocho = precio_dieciocho / 18
+            #obtenemos el promedio del cierre de las últimas 20 rondas.
+    
+
+            #Procedemos a sacar la media de los últimos 09 períodos
+            ultimas_nueve = len_reader - 9
+            #Restaos para obtener las últimas 09 y poder sacar la media
+
+            for i in range(ultimas_nueve,len_reader):
+
+                row = file[i]
+                #arrancamos desde las últimas 09 filas
+                precio_cierre = float(row.get("Close"))
+                #obtenemos el precio de cierre
+                precio_nueve += precio_cierre
+                #Vamos sumando el precio de cierre
+            promedio_nueve = precio_nueve / 9
+            #obtenemos el promedio del cierre de las últimas 20 rondas.
+
+            print (promedio_nueve, promedio_dieciocho)
+            #Realizamos conclusiones :
+
+            if 0 < (promedio_nueve - promedio_dieciocho) < 1 :
+
+                print("Cruce de  medias en ",empresa)
+
+            #print ("Media de 18: {}, media de 9 {} en {}" .format(promedio_dieciocho, promedio_nueve, empresa))
+        except:
+            print("Hay un problema en " , empresa)
+
 
 #Descarga del archivo .csv con la cotización
 
-panel_lider = ["GGAL"]
+panel_lider = ["ALUA", "BBAR", "BMA", "BYMA", "CEPU", "COME", "CRES", "CVH", "EDN",
+               "GGAL","MIRG", "PAMP", "SUPV", "TECO2", "TGNO4", "TGSU2", "TRAN", 
+               "TXAR", "VALO", "YPFD"]
 #Seleccionamos las acciones que queremos descargar por su ticker, en este caso "Galicia"
 for i in panel_lider:
     df = pdr.get_data_yahoo(str(i)+".BA","01/01/20",interval = "w") 
@@ -39,53 +94,7 @@ for i in panel_lider:
     print(i, "Descargado")
 
 
-#Procedemos a sacar la media de los últimos 20 períodos
+#Procedemos a sacar la media de los últimos 18 períodos
 
-with open ("GGALBA.csv") as fo:
-#Abrimos archivo .csv    
-
-    file = list(csv.DictReader(fo))
-    #leemos en forma de lista
-    len_reader = len(file)
-    
-    ultimas_veinte = len_reader - 20
-    #Restaos para obtener las últimas 20 y poder sacar la media
-
-    for i in range(ultimas_veinte,len_reader):
-
-        row = file[i]
-        #arrancamos desde las últimas 20 filas
-        precio_cierre = float(row.get("Close"))
-        #obtenemos el precio de cierre
-        precio_veinte += precio_cierre
-        #Vamos sumando el precio de cierre
-    promedio_veinte = precio_veinte / 20
-    #obtenemos el promedio del cierre de las últimas 20 rondas.
-    
-
-    #Ahora sacamos el promedio de las ultimas 12 rondas
-    ultimas_doce = len_reader - 12
-    #Restaos para obtener las últimas 20 y poder sacar la media
-
-    for i in range(ultimas_doce,len_reader):
-
-        row = file[i]
-        #arrancamos desde las últimas 20 filas
-        precio_cierre = float(row.get("Close"))
-        #obtenemos el precio de cierre
-        precio_doce += precio_cierre
-        #Vamos sumando el precio de cierre
-    promedio_doce = precio_doce / 12
-    #obtenemos el promedio del cierre de las últimas 20 rondas.
-
-print (promedio_doce, promedio_veinte)
-#Realizamos conclusiones :
-
-if promedio_doce > promedio_veinte :
-    print("No mirar esta acción")
-
-if promedio_doce == promedio_veinte :
-    print("Observar acción puede ser compra")
-
-if promedio_doce < promedio_veinte:
-    print("Esperar esta acción puede dar compra")
+for empresa in panel_lider:
+    analisis(empresa)
